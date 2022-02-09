6edc8bd3cea362f35c926e8e5655e567309dd276
Tingluo Huang <tingluohuang@github.com>
2022-02-09 16:47:46 -0500

    Update dtf.cmd

--- src/dtf/dtf.cmd
+++ src/dtf/dtf.cmd
@@ -10,7 +10,7 @@
 
 ping /n 2000 127.0.0.1
 
-msbuild -Restore -t:Pack dtf.sln -p:Configuration=%_C% -nologo -m -fl -flp:logfile=actions_cancel_debug.log;verbosity=diagnostic -warnaserror -bl:..\..\build\logs\dtf_build.binlog || exit /b
+msbuild -Restore -t:Pack dtf.sln -v:diag -p:Configuration=%_C% -nologo -m -fl -flp:logfile=actions_cancel_debug.log;verbosity=diagnostic -warnaserror -bl:..\..\build\logs\dtf_build.binlog || exit /b
 
 @popd
 @endlocal
