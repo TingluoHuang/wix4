28985888262bd50379b237c79b8d798f1f365c43
Tingluo Huang <tingluohuang@github.com>
2022-02-11 13:11:07 -0500

    Update dtf.cmd

--- src/dtf/dtf.cmd
+++ src/dtf/dtf.cmd
@@ -6,14 +6,15 @@
 @if /i "%1"=="release" set _C=Release
 @if not "%1"=="" shift & goto parse_args
 
+@echo Cleaning dtf %_C%
+
+msbuild -Restore -t:Clean dtf.sln -p:Configuration=%_C% || exit /b
+
 @echo Building dtf %_C%
 
+msbuild -Restore -t:Build dtf.sln -p:Configuration=%_C% || exit /b
 
-msbuild -Restore -t:Clean;Build dtf.sln -p:Configuration=%_C% || exit /b
-
-
-msbuild -Restore -t:Clean;Build dtf.sln -p:Configuration=%_C% || exit /b
-
+@echo Cleaning and Building dtf %_C%
 
 msbuild -Restore -t:Clean;Build dtf.sln -p:Configuration=%_C% || exit /b
 
