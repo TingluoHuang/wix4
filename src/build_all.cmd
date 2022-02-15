b15075ead6308796603f085e833d9c5ebfa2c8d8
Tingluo Huang <tingluohuang@github.com>
2022-02-15 15:33:09 -0500

    Update build_all.cmd

--- src/build_all.cmd
+++ src/build_all.cmd
@@ -7,13 +7,13 @@
 
 @echo build %_C%
 
-call build_init.cmd || exit /b
+call build_init.cmd || exit /b 1
 
 :: DTF
 
 where msbuild
 
-call dtf\dtf.cmd %_C% || exit /b
+call dtf\dtf.cmd %_C% || exit /b 1
 
 @popd
 @endlocal
